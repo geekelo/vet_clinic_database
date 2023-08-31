d26f7fde4dc7c7ef54b33a2fe49a9b7a428fd0f3
geekelo <efuelight12@gmail.com>
2023-08-31 15:34:13 +0100

    Add sql commands

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
-/*Queries that provide answers to the questions from all projects.*/
+/* Queries that provide answers to the questions from all projects. */
 
 SELECT * from animals WHERE name = 'Luna';
+-- Find all animals whose name ends in "mon":
+SELECT * from animals WHERE name LIKE '%mon';
