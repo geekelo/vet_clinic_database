e791c4a0c71393247f54d4c923563ba6811036a5
geekelo <efuelight12@gmail.com>
2023-08-31 18:35:43 +0100

    Add query command

--- queries.sql
+++ queries.sql
@@ -24,3 +24,11 @@ SELECT * from animals WHERE name <> 'Gabuwon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;  -- Verify changes
+ROLLBACK;               -- Roll back changes
+SELECT * FROM animals;  -- Verify changes reverted
+
